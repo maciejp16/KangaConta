6d5ce837f88fc4d4a3ab12cc6bb0923ff4a819de
Maciej Pięta <maciej.pieta16@gmail.com>
2019-03-02 12:52:32 +0100

    WIOSNA

---
+++ src/main/java/pl/teb/edukacja/KangaConta/KangaContaApplication.java
@@ -0,0 +1,13 @@
+package pl.teb.edukacja.KangaConta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KangaContaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KangaContaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/teb/edukacja/KangaConta/KangaContaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.teb.edukacja.KangaConta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KangaContaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
